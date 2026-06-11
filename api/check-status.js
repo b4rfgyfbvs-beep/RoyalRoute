20fd2dedc6ddfa620426a147f57ea289bc8e9920
b4rfgyfbvs-beep <b4rfgyfbvs@privaterelay.appleid.com>
2026-06-10 21:19:17 -0500

    Create check-status.js

---
+++ api/check-status.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const siteOpen = process.env.SITE_OPEN !== 'false';
+  const spotsTaken = parseInt(process.env.SPOTS_TAKEN || '0');
+  const totalSpots = 5;
+  return res.status(200).json({
+    siteOpen,
+    spotsTaken,
+    totalSpots,
+    spotsRemaining: totalSpots - spotsTaken,
+  });
+};
