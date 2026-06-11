dfaaba62b51c0cc0ea344c22243b793603fa410b
b4rfgyfbvs-beep <b4rfgyfbvs@privaterelay.appleid.com>
2026-06-10 23:51:55 -0500

    Update check-status.js

--- check-status.js
+++ check-status.js
@@ -1,13 +1,11 @@
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-  const siteOpen = process.env.SITE_OPEN !== 'false';
-  const spotsTaken = parseInt(process.env.SPOTS_TAKEN || '0');
-  const totalSpots = 5;
+  res.setHeader('Cache-Control', 'no-store');
   return res.status(200).json({
-    siteOpen,
-    spotsTaken,
-    totalSpots,
-    spotsRemaining: totalSpots - spotsTaken,
+    siteOpen: false,
+    spotsTaken: 5,
+    totalSpots: 5,
+    spotsRemaining: 0,
   });
 };
+
